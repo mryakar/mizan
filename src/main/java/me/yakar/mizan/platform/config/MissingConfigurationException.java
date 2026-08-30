b242612c0a62460f038af8e3d5cad678094155db
Amo <ahmet@yakar.me>
2026-08-30 19:57:37 +0300

    chore: add MissingConfigurationException for handling missing configuration scenarios

---
+++ src/main/java/me/yakar/mizan/platform/config/MissingConfigurationException.java
@@ -0,0 +1,8 @@
+package me.yakar.mizan.platform.config;
+
+public class MissingConfigurationException extends RuntimeException {
+
+  public MissingConfigurationException(String message) {
+    super(message);
+  }
+}
